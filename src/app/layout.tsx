e1ffedb094d946400c3cd153e0943c5654cd5eb5
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-09-10 21:18:04 +0300

    Background set up

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+import  './globals.css';
+export default function RootLayout() {
+  return (
+    <html lang="en">
+      <body>
+        <div id="long">
+          
+        </div>
+        <div id="wave">
+        </div>
+      </body>
+    </html>
+  );
+}
