09741d061ca34ea210fc953c5968434789d2ea33
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-09-16 19:15:48 +0300

    Tiny stats created.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 import styles from "./page.module.css";
 import Layout from "./layout/layout";
 import SmGraphs from "./small-graphs/smGraphs";
+import TinyGraph from "./tiny-graph/tiny-graph";
 export default function Home() {
   return (
     <div className={styles.main}>
       <Layout />
-      <SmGraphs/>
+      <SmGraphs />
+      <TinyGraph/>
     </div>
   );
 }
--- src/app/tiny-graph/tiny-graph.tsx
+++ src/app/tiny-graph/tiny-graph.tsx
@@ -1,7 +1,34 @@
 import styles from './tiny-graph.module.css';
-export default function tinyGraph() {
+export default function TinyGraph() {
   return (
-  <div className={styles.main}>
-    
-  </div>)
+    <div className={styles.main}>
+    <div className={styles.row}>
+    <div className={styles.box}>
+        
+    </div>
+    <div className={styles.box}>
+        
+    </div>
+    <div className={styles.box}>
+        
+    </div>
+    <div className={styles.box}>
+        
+    </div>
+    </div>
+    <div className={styles.row}>
+    <div className={styles.box}>
+        
+    </div>  
+    <div className={styles.box}>
+        
+    </div>
+    <div className={styles.box}>
+        
+    </div>
+    <div className={styles.box}>
+        
+    </div>
+    </div>
+    </div>)
 } 
