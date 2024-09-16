42792d15e7731bef76788ac0b0650781e9f7441b
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-09-16 19:29:10 +0300

    Tiny stats title created.

--- src/app/tiny-graph/tiny-graph.tsx
+++ src/app/tiny-graph/tiny-graph.tsx
@@ -1,33 +1,38 @@
 import styles from './tiny-graph.module.css';
+import Image from 'next/image';
+import fbicon from '../images/icon-facebook.svg';
+import twicon from '../images/icon-twitter.svg';
+import igicon from '../images/icon-instagram.svg';
+import yticon from '../images/icon-youtube.svg';
 export default function TinyGraph() {
   return (
     <div className={styles.main}>
     <div className={styles.row}>
     <div className={styles.box}>
-        
+      <div className={styles.title}>Page views <Image src={fbicon} alt=''/></div>
     </div>
     <div className={styles.box}>
-        
+      <div className={styles.title}>Likes <Image src={fbicon} alt=''/></div>
     </div>
     <div className={styles.box}>
-        
+      <div className={styles.title}>Likes <Image src={igicon} alt=''/></div>
     </div>
     <div className={styles.box}>
-        
+      <div className={styles.title}>Profile Views <Image src={igicon} alt=''/></div>    
     </div>
     </div>
     <div className={styles.row}>
     <div className={styles.box}>
-        
+      <div className={styles.title}>Retweets<Image src={twicon} alt=''/></div>  
     </div>  
     <div className={styles.box}>
-        
+      <div className={styles.title}>Likes<Image src={twicon} alt=''/></div>
     </div>
     <div className={styles.box}>
-        
+      <div className={styles.title}>Likes<Image src={yticon} alt=''/></div>  
     </div>
     <div className={styles.box}>
-        
+      <div className={styles.title}>Total Views<Image src={yticon} alt=''/></div>
     </div>
     </div>
     </div>)
