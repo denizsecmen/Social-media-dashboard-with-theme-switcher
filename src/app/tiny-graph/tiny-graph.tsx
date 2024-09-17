3bbd3cf0a74cd4150b4ec99101b4b8f878109880
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-09-17 22:10:18 +0300

    Tiny stats data created.

--- src/app/tiny-graph/tiny-graph.tsx
+++ src/app/tiny-graph/tiny-graph.tsx
@@ -9,30 +9,38 @@ export default function TinyGraph() {
     <div className={styles.main}>
     <div className={styles.row}>
     <div className={styles.box}>
-      <div className={styles.title}>Page views <Image src={fbicon} alt=''/></div>
+          <div className={styles.title}>Page views <Image src={fbicon} alt='' /></div>
+          <div className={styles.data}>87<div className={styles.icon} id="fbdatav"></div></div>
     </div>
     <div className={styles.box}>
-      <div className={styles.title}>Likes <Image src={fbicon} alt=''/></div>
+          <div className={styles.title}>Likes <Image src={fbicon} alt='' /></div>
+          <div className={styles.data}>52<div className={styles.icon} id="fbdataL"></div></div>
     </div>
     <div className={styles.box}>
-      <div className={styles.title}>Likes <Image src={igicon} alt=''/></div>
+          <div className={styles.title}>Likes <Image src={igicon} alt='' /></div>
+          <div className={styles.data}>5462<div className={styles.icon} id="igdataL"></div></div>
     </div>
     <div className={styles.box}>
-      <div className={styles.title}>Profile Views <Image src={igicon} alt=''/></div>    
+          <div className={styles.title}>Profile Views <Image src={igicon} alt='' /></div>
+          <div className={styles.data}>52k<div className={styles.icon} id="igdataPW"></div></div>
     </div>
     </div>
     <div className={styles.row}>
     <div className={styles.box}>
-      <div className={styles.title}>Retweets<Image src={twicon} alt=''/></div>  
+          <div className={styles.title}>Retweets<Image src={twicon} alt='' /></div>  
+          <div className={styles.data}>117<div className={styles.icon} id="twdataRT"></div></div>
     </div>  
     <div className={styles.box}>
-      <div className={styles.title}>Likes<Image src={twicon} alt=''/></div>
+          <div className={styles.title}>Likes<Image src={twicon} alt='' /></div>
+          <div className={styles.data}>507<div className={styles.icon} id="twdataL"></div></div>
     </div>
     <div className={styles.box}>
-      <div className={styles.title}>Likes<Image src={yticon} alt=''/></div>  
+          <div className={styles.title}>Likes<Image src={yticon} alt='' /></div>
+          <div className={styles.data}>107<div className={styles.icon} id="ytdataL"></div></div>
     </div>
     <div className={styles.box}>
-      <div className={styles.title}>Total Views<Image src={yticon} alt=''/></div>
+          <div className={styles.title}>Total Views<Image src={yticon} alt='' /></div>
+          <div className={styles.data}>1407<div className={styles.icon} id="ytdataPW"></div></div>
     </div>
     </div>
     </div>)
