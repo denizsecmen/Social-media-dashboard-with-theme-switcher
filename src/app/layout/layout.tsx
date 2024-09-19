b310367735c2f059ab53256c3437cc758166204c
Deniz Seçmen <denizsecmen1240@gmail.com>
2024-09-19 23:26:30 +0300

    Switrch configured

--- src/app/layout/layout.tsx
+++ src/app/layout/layout.tsx
@@ -9,8 +9,21 @@ export default function Layout() {
         <div className={styles.data}>Total Followers: 23,004</div> 
       </div>
       <div className={styles.switch}>
-        <p>Dark Mode</p>
-        <Switch className={styles.switch} display="Flex" colorScheme='teal' ></Switch>
+        <div>Dark Mode</div>
+        <Switch sx={{
+          marginLeft: "10px",
+          marginBottom:"5px",
+          width: "100px",
+          height:"15px",
+        '& .chakra-switch__track': {
+          height: '15px',
+          width:'30px',// Adjust track height if needed
+        },
+        '& .chakra-switch__thumb': {
+          width: '15px', // Adjust thumb width if needed
+          height: '15px', // Adjust thumb height if needed
+        },
+      }}  display="Flex" colorScheme='teal' ></Switch>
       </div>  
     </div>
   );
